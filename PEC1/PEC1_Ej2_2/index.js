0ce8ed961ea1f6dcf512eda08d6344934c9dba93
Jordi Tobajas <jtobajasa@uoc.edu>
2023-10-15 15:31:16 +0200

    Ej2_2 index.js finished.

--- PEC1/PEC1_Ej2_2/index.js
+++ PEC1/PEC1_Ej2_2/index.js
@@ -6,6 +6,8 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
+const load = document.getElementById('load');
+
 //  Add min atribute to inputs
 amountEl_one.setAttribute("min", "0");
 amountEl_two.setAttribute("min", "0");
@@ -15,12 +17,15 @@ function calculate() {
     const currency_one = currencyEl_one.value;
     const currency_two = currencyEl_two.value;
 
+    load.classList.toggle('now');
+    
     fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
         .then(res => res.json())
         .then(data => {
             const rate = data.rates[currency_two];
             rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
             amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+            load.classList.remove('now');
         })
         .catch(err => {
             if(typeof err.json === "function") {
@@ -36,6 +41,7 @@ function calculate() {
                 console.log(err);
             }
         });
+    
 }
 
 // Event listeners
